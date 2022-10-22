8993b3e4ad621999c131b6a0180dcacb4219b1f9
Aoufi Abderhmane <Abdouou7@gmail.com>
2022-10-22 14:20:40 +0100

    finish stories

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
 import Navbar from "../../components/Navbar/Navbar";
+import StoryContainer from "./StoryContainer";
 
 const Home = () => {
   return (
     <dvi>
       <Navbar />
+      <div className="flex px-24">
+        <div>
+          <StoryContainer />
+        </div>
+        <div>
+          // info
+        </div>
+      </div>
     </dvi>
   );
 };
---
+++ src/pages/home/StoryContainer.jsx
@@ -0,0 +1,56 @@
+const stories = [
+  {
+    name: "John",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "mike",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "filip",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "max",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "filip",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "max",
+    imgSrc:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+];
+
+const SingleStory = (props) => {
+  return (
+    <div className="h-16 w-16 mx-2 ">
+      <img
+        className="rounded-full h-full w-full cover border border-2 border-red-500 p-1"
+        src={props.imgSrc}
+      />
+      <p className="text-center">{props.name || "unknown user"}</p>
+    </div>
+  );
+};
+
+const StoryContainer = () => {
+  return (
+    <div className="flex p-4 border-2 mt-6a">
+      {stories.map((e) => {
+        return <SingleStory name={e.name} imgSrc={e.imgSrc} />;
+      })}
+    </div>
+  );
+};
+
+export default StoryContainer;
